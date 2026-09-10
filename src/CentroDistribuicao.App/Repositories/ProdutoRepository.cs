f7c1f5a71eb25f3313ed055d6fd426b33e176d78
LUCAS DE SABOIA FROTA <146458952+lukasdesaboia@users.noreply.github.com>
2026-09-10 19:35:12 -0300

    Adiciona consulta de produtos

---
+++ src/CentroDistribuicao.App/Repositories/ProdutoRepository.cs
@@ -0,0 +1,45 @@
+using CentroDistribuicao.App.Data;
+using CentroDistribuicao.App.Models;
+
+namespace CentroDistribuicao.App.Repositories;
+
+public class ProdutoRepository
+{
+    public async Task<List<Produto>> ListarAsync()
+    {
+        const string sql = """
+            SELECT
+                IdProduto,
+                Codigo,
+                Descricao,
+                Unidade,
+                Ativo
+            FROM dbo.Produtos
+            ORDER BY Descricao;
+            """;
+
+        var produtos = new List<Produto>();
+
+        await using var conexao = ConexaoBanco.Criar();
+        await conexao.OpenAsync();
+
+        await using var comando = conexao.CreateCommand();
+        comando.CommandText = sql;
+
+        await using var leitor = await comando.ExecuteReaderAsync();
+
+        while (await leitor.ReadAsync())
+        {
+            produtos.Add(new Produto
+            {
+                IdProduto = leitor.GetInt32(0),
+                Codigo = leitor.GetString(1),
+                Descricao = leitor.GetString(2),
+                Unidade = leitor.GetString(3),
+                Ativo = leitor.GetBoolean(4)
+            });
+        }
+
+        return produtos;
+    }
+}

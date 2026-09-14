4e7d770743f7d31345cb514d62d3d8cb82ea4b42
LUCAS DE SABOIA FROTA <146458952+lukasdesaboia@users.noreply.github.com>
2026-09-14 16:37:27 -0300

    Adiciona modelo de item de entrada

---
+++ src/CentroDistribuicao.App/Models/EntradaItemCadastro.cs
@@ -0,0 +1,10 @@
+namespace CentroDistribuicao.App.Models;
+
+public class EntradaItemCadastro
+{
+    public int IdProduto { get; set; }
+
+    public decimal Quantidade { get; set; }
+
+    public decimal? ValorUnitario { get; set; }
+}
